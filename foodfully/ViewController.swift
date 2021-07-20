f0ef553c932aaea55bd586f851bf8ea5ce17d074
tifli <tiffanyli5511@gmail.com>
2021-07-20 13:20:58 -0700

    Initial Commit

---
+++ foodfully/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  foodfully
+//
+//  Created by Tiffany Li on 7/20/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
